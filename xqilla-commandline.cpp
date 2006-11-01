dadb86cf4b3a852035fe3979344a5e5731d81f4a
John Snelson <jpcs@users.sourceforge.net>
2006-11-01 16:37:23 +0000

    Updated the copyright notices to say Oracle, instead of Sleeycat Software.

--- xqilla-commandline.cpp
+++ xqilla-commandline.cpp
@@ -4,7 +4,7 @@
  * Copyright (c) 2004-2006
  *     Progress Software Corporation. All rights reserved.
  * Copyright (c) 2004-2006
- *     Sleepycat Software. All rights reserved.
+ *     Oracle. All rights reserved.
  *
  * See the file LICENSE for redistribution information.
  *
