9d3787a305a04fd07d1f533c7039f9f72cf6b531
John Snelson <jpcs@users.sourceforge.net>
2009-01-12 15:02:54 +0000

    Updated the copyright notices to 2009.

--- xqilla-commandline.cpp
+++ xqilla-commandline.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2001-2008
  *     DecisionSoft Limited. All rights reserved.
- * Copyright (c) 2004-2008
+ * Copyright (c) 2004-2009
  *     Oracle. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,8 +15,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * $Id$
  */
 
 #include <iostream>
